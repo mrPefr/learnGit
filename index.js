bdec3068d690ad4b557f93766e0fa023271a1f6b
mrPefr <mrpefr@gmail.com>
2024-11-26 09:53:16 +0100

    kommentarer skapade

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 
 app.listen(3400);
 
+// Min hem-route
 app.get("/", (req, res)=>{
     res.send("HOME");
 })
 
+// Min aobut-route
 app.get("/about", (req, res)=>{
     res.send("ABOUT");
 })
 
+// min viktiga route.....
 app.get("/important", (req, res)=>{
     res.send("Important stuff....");
 })
